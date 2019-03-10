aacab8ac29dfaabcd68c78f3b853bb9f556fbd02
Deseteral <deseteral@gmail.com>
2019-03-10 21:58:58 +0100

    DirectoryTree component

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,41 +1,15 @@
 import * as React from 'react';
-import { RepositoryData, Directory } from '../domain';
+import { RepositoryData } from '../domain';
+import DirectoryTree from './DirectoryTree';
 
 interface DashboardProps {
   repoData: RepositoryData;
 }
 
-function renderNode(currentDir: Directory, childDirs: Directory[], dirs: Directory[]) {
-  const WrapperNode: (props: any) => JSX.Element = currentDir.id === 'root'
-    ? props => <div {...props} />
-    : props => <li {...props} />;
-
-  return (
-    <WrapperNode key={currentDir.id}>
-      {currentDir.name && currentDir.name}
-      {(childDirs.length > 0) && (
-        <ul>
-          {childDirs.map(child => (
-            renderNode(child, dirs.filter(d => d.parent === child.id), dirs)
-          ))}
-        </ul>
-      )}
-    </WrapperNode>
-  );
-}
-
 function Dashboard({ repoData } : DashboardProps) : JSX.Element {
-  const rootDirs = repoData.directories.filter(directory => directory.parent === 'root');
-
-  const rootDir: Directory = {
-    id: 'root',
-    name: '',
-    parent: '',
-  };
-
   return (
     <div>
-      {renderNode(rootDir, rootDirs, repoData.directories)}
+      <DirectoryTree directories={repoData.directories} />
     </div>
   );
 }
